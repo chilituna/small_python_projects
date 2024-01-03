84d22e01c1db0fa930fe1b72d3f9589cf520566b
Alise <133121967+chilituna@users.noreply.github.com>
2024-01-03 22:20:12 +0100

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,53 @@
+#Play Rock Paper Scissors against computer
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+images = [rock, paper, scissors]
+
+choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+choice = int(choice)
+
+if choice >= 0 and choice <= 2:
+  print(images[choice])
+else:
+  print("That's not an option. You lose.")
+
+print("\nComputer chooses:")
+
+computer = random.randint(0, 2)
+print(images[computer])
+
+if choice == computer:
+  print("It's a tie!")
+elif (choice < 2 and computer == choice + 1) or (choice == 2 and computer == 0):
+  print ("You lose!")
+else:
+  print ("You win!")
+exit()
