340d76a1de36210661cf2802eba6927f83c54edf
Alise <133121967+chilituna@users.noreply.github.com>
2024-10-21 13:56:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, coding):
+    encrypted_text = ''
+    if coding == 'decode':
+        shift_amount *= -1
+    for letter in original_text:
+        if letter in alphabet:
+            new_position = alphabet.index(letter) + shift_amount
+            new_position %= len(alphabet)
+            encrypted_text += alphabet[new_position]
+        else:
+            encrypted_text += letter
+
+    print(f"Here is the {coding}d result: {encrypted_text}\n")
+
+def run_program():
+    looping = True
+    while looping:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+        while direction != 'encode' and direction != 'decode':
+            print("Incorrect input. Please try again.\n")
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(text, shift, direction)
+        if input("Type 'yes' to continue. Type anything else to quit:\n").lower() != 'yes':
+            looping = False
+
+run_program()
+
+
+
+
+
+
+
+
